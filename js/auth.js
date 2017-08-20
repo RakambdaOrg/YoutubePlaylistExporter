d727ffae8788b2dfd31d5b0e665eba90232f6a9f
Thomas Couchoud <zerderr@gmail.com>
2017-08-20 12:40:03 +0200

    Cleanup

--- js/auth.js
+++ js/auth.js
@@ -1,67 +1,65 @@
 var OAUTH2_CLIENT_ID = '850390026134-6u673fslj5c3de9b7oqaouiuj3qg3ina.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
-    'https://www.googleapis.com/auth/youtube'
+	'https://www.googleapis.com/auth/youtube'
 ];
 
 $(document).ready(function () {
-   $('#logoff').click(logout);
+	$('#logoff').click(logout);
 });
 
 function logout() {
-    var token = gapi.auth.getToken();
-    if (token)
-    {
-        var accessToken = gapi.auth.getToken().access_token;
-        if (accessToken)
-        {
-            $.ajax({
-                'url': 'https://accounts.google.com/o/oauth2/revoke?token=' + accessToken,
-                'async': true,
-                'method': 'GET',
-                'success':function(data){
-                    $('.pre-auth').show();
-                    $('.post-auth').hide();
-                }
-            });
-        }
-    }
-    gapi.auth.setToken(null);
-    gapi.auth.signOut();
-    console.log("signing off");
+	var token = gapi.auth.getToken();
+	if (token) {
+		var accessToken = gapi.auth.getToken().access_token;
+		if (accessToken) {
+			$.ajax({
+				'url': 'https://accounts.google.com/o/oauth2/revoke?token=' + accessToken,
+				'async': true,
+				'method': 'GET',
+				'success': function () {
+					$('.pre-auth').show();
+					$('.post-auth').hide();
+				}
+			});
+		}
+	}
+	gapi.auth.setToken(null);
+	gapi.auth.signOut();
+	console.log("signing off");
 }
 
-googleApiClientReady = function() {
-    gapi.auth.init(function() {
-        window.setTimeout(checkAuth, 1);
-    });
+googleApiClientReady = function () {
+	gapi.auth.init(function () {
+		window.setTimeout(checkAuth, 1);
+	});
 };
 
 function checkAuth() {
-    gapi.auth.authorize({
-        client_id: OAUTH2_CLIENT_ID,
-        scope: OAUTH2_SCOPES,
-        immediate: true
-    }, handleAuthResult);
+	gapi.auth.authorize({
+		client_id: OAUTH2_CLIENT_ID,
+		scope: OAUTH2_SCOPES,
+		immediate: true
+	}, handleAuthResult);
 }
 
 function handleAuthResult(authResult) {
-    if (authResult && !authResult.error) {
-        $('.pre-auth').hide();
-        $('.post-auth').show();
-        loadAPIClientInterfaces();
-    } else {
-        $('#login-link').click(function() {
-            gapi.auth.authorize({
-                client_id: OAUTH2_CLIENT_ID,
-                scope: OAUTH2_SCOPES,
-                immediate: false
-            }, handleAuthResult);
-        });
-    }
+	if (authResult && !authResult.error) {
+		$('.pre-auth').hide();
+		$('.post-auth').show();
+		loadAPIClientInterfaces();
+	} else {
+		$('#login-link').click(function () {
+			gapi.auth.authorize({
+				client_id: OAUTH2_CLIENT_ID,
+				scope: OAUTH2_SCOPES,
+				immediate: false
+			}, handleAuthResult);
+		});
+	}
 }
 
 function loadAPIClientInterfaces() {
-    gapi.client.load('youtube', 'v3', function() {
-        handleAPILoaded();
-    });
+	gapi.client.load('youtube', 'v3', function () {
+		handleAPILoaded();
+	});
 }
--- js/playlist.js
+++ js/playlist.js
@@ -1,121 +1,112 @@
-$(document).ready(function(){
-   $('#load-container').hide();
+$(document).ready(function () {
+	$('#load-container').hide();
 });
 
 function handleAPILoaded() {
-    requestUserUploadsPlaylistId();
+	requestUserUploadsPlaylistId();
 }
 
 function exportPlaylists(id) {
-    $('#playlist-container').hide();
-    $('#load-container').show();
-    getVideosLinks(id, function (result) {
-        $('#load-container').hide();
-        var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(result));
-        var button = document.createElement('button');
-        button.innerHTML = '<a class="downloadJSON" download="data.json" href="data:' + data + '">Download JSON</a>';
-        var container = document.getElementById('download-container');
-        container.appendChild(button);
-    });
+	$('#playlist-container').hide();
+	$('#load-container').show();
+	getVideosLinks(id, function (result) {
+		$('#load-container').hide();
+		var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(result));
+		var button = document.createElement('button');
+		button.innerHTML = '<a class="downloadJSON" download="data.json" href="data:' + data + '">Download JSON</a>';
+		var container = document.getElementById('download-container');
+		container.appendChild(button);
+	});
 }
 
 function requestUserUploadsPlaylistId() {
-    $('#load-container').show();
-   getPlaylists(function (result) {
-       $('#load-container').hide();
-       $.each(result, function(index, item) {
-           $('#playlist-container').append('<p class="playlistContainer"><button type="button" class="playlist-button" id="' + item.id + '">' + item.title + '</button></p>');
-           $('#' + item.id).click(function () {
-               exportPlaylists($(this).attr('id'));
-           })
-       });
-   });
+	$('#load-container').show();
+	getPlaylists(function (result) {
+		$('#load-container').hide();
+		$.each(result, function (index, item) {
+			$('#playlist-container').append('<p class="playlistContainer"><button type="button" class="playlist-button" id="' + item.id + '">' + item.title + '</button></p>');
+			$('#' + item.id).click(function () {
+				exportPlaylists($(this).attr('id'));
+			})
+		});
+	});
 }
 
-function getPlaylists(callback, pageToken)
-{
-    console.log("Getting playlist list");
-    var requestOptions = {
-        part: 'snippet',
-        mine: true,
-        maxResults: 50
-    };
-    if (pageToken) {
-        requestOptions.pagetoken = pageToken;
-        console.log("Page token is " + pageToken);
-    }
-    var request = gapi.client.youtube.playlists.list(requestOptions);
-    request.execute(function(response) {
-        //console.log(response);
-        var nextPageToken = response.result.nextPageToken;
-        var playlistItems = response.result.items;
-        var playlists = [];
-        if(playlistItems)
-        {
-            for(var i = 0; i < playlistItems.length; i+=1)
-            {
-                playlists.push({id:playlistItems[i].id, title:playlistItems[i].snippet.title});
-            }
-        }
-        if(nextPageToken)
-        {
-            getVideosLinks(function (result) {
-                playlists.push.apply(playlists, result);
-                callback(playlists);
-            }, nextPageToken);
-        }
-        else
-        {
-            playlists.sort(function(a, b){
-               return a.title.localeCompare(b.title);
-            });
-            callback(playlists);
-        }
-    }, function (error) {
-        console.log("Error:");
-        console.log(error);
-        callback([]);
-    });
+function getPlaylists(callback, pageToken) {
+	console.log("Getting playlist list");
+	var requestOptions = {
+		part: 'snippet',
+		mine: true,
+		maxResults: 50
+	};
+	if (pageToken) {
+		requestOptions.pagetoken = pageToken;
+		console.log("Page token is " + pageToken);
+	}
+	var request = gapi.client.youtube.playlists.list(requestOptions);
+	request.execute(function (response) {
+		//console.log(response);
+		var nextPageToken = response.result.nextPageToken;
+		var playlistItems = response.result.items;
+		var playlists = [];
+		if (playlistItems) {
+			for (var i = 0; i < playlistItems.length; i += 1) {
+				playlists.push({id: playlistItems[i].id, title: playlistItems[i].snippet.title});
+			}
+		}
+		if (nextPageToken) {
+			getVideosLinks(function (result) {
+				playlists.push.apply(playlists, result);
+				callback(playlists);
+			}, nextPageToken);
+		}
+		else {
+			playlists.sort(function (a, b) {
+				return a.title.localeCompare(b.title);
+			});
+			callback(playlists);
+		}
+	}, function (error) {
+		console.log("Error:");
+		console.log(error);
+		callback([]);
+	});
 }
 
 function getVideosLinks(playlistId, callback, pageToken) {
-    console.log("Getting playlist info");
-    var requestOptions = {
-        playlistId: playlistId,
-        part: 'snippet',
-        maxResults: 50
-    };
-    if (pageToken) {
-        requestOptions.pageToken = pageToken;
-        console.log("Page token is " + pageToken);
-    }
-    var request = gapi.client.youtube.playlistItems.list(requestOptions);
-    request.execute(function(response) {
-        //console.log(response);
-        var nextPageToken = response.result.nextPageToken;
-        var playlistItems = response.result.items;
-        var links = [];
-        if(playlistItems)
-        {
-            for(var i = 0; i < playlistItems.length; i+=1)
-            {
-                links.push(playlistItems[i].snippet.resourceId.videoId);
-            }
-        }
-        if(nextPageToken)
-        {
-            getVideosLinks(playlistId, function (result) {
-                links.push.apply(links, result);
-                callback(links);
-            }, nextPageToken);
-        }
-        else
-        {
-            callback(links);
-        }
-    }, function (error) {
-        console.log("Error:");
-        console.log(error);
-        callback([]);
-    });
+	console.log("Getting playlist info");
+	var requestOptions = {
+		playlistId: playlistId,
+		part: 'snippet',
+		maxResults: 50
+	};
+	if (pageToken) {
+		requestOptions.pageToken = pageToken;
+		console.log("Page token is " + pageToken);
+	}
+	var request = gapi.client.youtube.playlistItems.list(requestOptions);
+	request.execute(function (response) {
+		//console.log(response);
+		var nextPageToken = response.result.nextPageToken;
+		var playlistItems = response.result.items;
+		var links = [];
+		if (playlistItems) {
+			for (var i = 0; i < playlistItems.length; i += 1) {
+				links.push(playlistItems[i].snippet.resourceId.videoId);
+			}
+		}
+		if (nextPageToken) {
+			getVideosLinks(playlistId, function (result) {
+				links.push.apply(links, result);
+				callback(links);
+			}, nextPageToken);
+		}
+		else {
+			callback(links);
+		}
+	}, function (error) {
+		console.log("Error:");
+		console.log(error);
+		callback([]);
+	});
 }
